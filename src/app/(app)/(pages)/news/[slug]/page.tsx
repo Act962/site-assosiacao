7950f2a46535d21d8f92511efe7caf5c7dc706a7
João Gabriel <joaogabriel9633@gmail.com>
2026-01-22 08:56:22 -0300

    update SEO

--- src/app/(app)/(pages)/news/[slug]/page.tsx
+++ src/app/(app)/(pages)/news/[slug]/page.tsx
@@ -1,11 +1,39 @@
 import { prefetchNew } from "@/modules/news/server/prefetch";
 import { NewsDetailPage } from "@/templates/new-detail";
-import { HydrateClient } from "@/trpc/server";
+import { caller, HydrateClient } from "@/trpc/server";
 import { Suspense } from "react";
+import { Metadata } from "next";
 import { ErrorBoundary } from "react-error-boundary";
 
 interface NewsDetailProps {
   params: Promise<{ slug: string }>;
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}): Promise<Metadata> {
+  const { slug } = await params;
+
+  const newDetail = await caller.news.getOne({
+    slug,
+  });
+
+  const article = newDetail.docs[0];
+
+  if (!article) {
+    return {};
+  }
+
+  return {
+    title: article.title,
+    description: article.excerpt,
+    robots: "index, follow",
+    openGraph: {
+      images: [article.coverImage?.url || ""],
+    },
+  };
 }
 
 export default async function NewsDetail({ params }: NewsDetailProps) {
