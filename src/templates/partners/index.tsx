08f78c85c8a6c52f23a21d592245988e69bf02ac
João Gabriel <joaogabriel9633@gmail.com>
2026-01-26 17:24:01 -0300

    grid cols partner update

--- src/templates/partners/index.tsx
+++ src/templates/partners/index.tsx
@@ -97,7 +97,7 @@ export default function PartnersPage() {
               Nenhum parceiro encontrado
             </div>
           ) : (
-            <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 mx-auto place-content-center gap-y-12">
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 mx-auto place-content-center gap-y-12">
               {partners.map((partner) => (
                 <Link
                   key={partner.id}
